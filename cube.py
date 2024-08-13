4ef3d7c536942957e28e587c97f58d052877a7c7
disignetango <53143179+disignetango@users.noreply.github.com>
2024-08-13 12:38:18 +0200

    Update cube.py

--- cube.py
+++ cube.py
@@ -6,7 +6,6 @@ def generate_codes(prefix, num_codes=10):
         suffix = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=16))
         codes.append(prefix + '-' + suffix)
     return codes
-
-# Exécution et affichage des 10 codes générés
+    
 for code in generate_codes('CUBE'):
     print(code)
